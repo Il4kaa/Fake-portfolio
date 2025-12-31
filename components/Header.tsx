9ed8421b107594546960bc3902e24702605f963a
Il4kaa <iliaovinnikov@gmail.com>
2025-12-31 12:51:33 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,14 +14,15 @@ export default function Header({logoText}: {logoText: string} ) {
             <button onClick={() => setIsOpen(!isOpen)} className="md:hidden z-50 p-2 text-white hover:text-blue-500 duration-500 transition">{isOpen ? <X size={35} /> : <Menu size={35} />}</button>
 
             <nav className={`fixed md:static inset-0 h-screen md:h-auto w-full md:w-auto bg-neutral-950 md:bg-transparent flex flex-col md:flex-row items-center justify-center md:justify-end gap-10 md:gap-24 duration-500 ease-in-out transition-all ${isOpen ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0 md:translate-x-0 md:opacity-100'}`}>
-              <a onClick={() => setIsOpen(false)} href="#about" className="text-2xl md:text-base text-neutral-300 font-mono shadow-2xl hover:text-blue-500 duration-500 transition">Обо мне</a>
-              <a onClick={() => setIsOpen(false)} href="#skills" className="text-2xl md:text-base text-neutral-300 font-mono shadow-2xl hover:text-blue-500 duration-500 transition">Навыки</a>
-              <a onClick={() => setIsOpen(false)} href="#testimonials" className="text-2xl md:text-base text-neutral-300 font-mono shadow-2xl hover:text-blue-500 duration-500 transition">Отзывы</a>
-              <a onClick={() => setIsOpen(false)} href="#projects" className="text-2xl md:text-base text-neutral-300 font-mono shadow-2xl hover:text-blue-500 duration-500 transition">Проекты</a>
-              <a onClick={() => setIsOpen(false)} href="#contact" className="text-2xl md:text-base text-neutral-300 font-mono shadow-2xl hover:text-blue-500 duration-500 transition">Контакты</a>
+              <a onClick={() => setIsOpen(false)} href="#about" className="text-2xl md:text-base text-neutral-300 font-mono shadow-2xl hover:text-blue-500 duration-300 transition">Обо мне</a>
+              <a onClick={() => setIsOpen(false)} href="#skills" className="text-2xl md:text-base text-neutral-300 font-mono shadow-2xl hover:text-blue-500 duration-300 transition">Навыки</a>
+              <a onClick={() => setIsOpen(false)} href="#testimonials" className="text-2xl md:text-base text-neutral-300 font-mono shadow-2xl hover:text-blue-500 duration-300 transition">Отзывы</a>
+              <a onClick={() => setIsOpen(false)} href="#projects" className="text-2xl md:text-base text-neutral-300 font-mono shadow-2xl hover:text-blue-500 duration-300 transition">Проекты</a>
+              <a onClick={() => setIsOpen(false)} href="#contact" className="text-2xl md:text-base text-white font-mono shadow-2xl duration-300 transition bg-blue-600 rounded-[2rem] hover:bg-blue-500 p-4">Контакты</a>
             </nav>
 
           </div>
         </header>
     );
 }
+
