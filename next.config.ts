98a0864bfa008c6272a432ca29acf60f9a8dd0fb
Il4kaa <iliaovinnikov@gmail.com>
2026-01-01 11:01:39 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,3 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  /* config options here */
-};
-
+/** @type {import('next').NextConfig} */
+const nextConfig = { output: 'export', images: { unoptimized: true } };
 export default nextConfig;
